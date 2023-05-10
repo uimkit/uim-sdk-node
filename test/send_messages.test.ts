8f6938372079dcee7c929197976eb1f59e5b2b12
fioman <fioman@qq.com>
2023-05-11 02:33:54 +0800

    typo

--- test/send_messages.test.ts
+++ test/send_messages.test.ts
@@ -50,7 +50,7 @@ describe('send_messages', () => {
 
   // it('send image message to conversation', async () => {
   //   const sendReq = client.createImageMessage({
-  //     conversation_id: 'dtHhdHwjSZ56qxdADL8q3',
+  //     conversation_id: 'kixVL6qOfz9xLcPe_xzpA',
   //     upload_file: 'test/resources/test_image.jpeg',
   //   });
   //   const message = await client.sendMessage(sendReq);
@@ -85,23 +85,23 @@ describe('send_messages', () => {
   //   console.log(JSON.stringify(message, undefined, 4));
   // });
 
-  // it('send miniprogram message to conversation', async () => {
-  //   const sendReq = client.createMiniProgramMessage({
-  //     conversation_id: 'kixVL6qOfz9xLcPe_xzpA',
-  //     miniprogram: {
-  //       content: "<?xml version=\"1.0\"?>\n<msg>\n\t<appmsg appid=\"\" sdkver=\"0\">\n\t\t<title>优衣云选服装女装批发进货</title>\n\t\t<des />\n\t\t<username />\n\t\t<action>view</action>\n\t\t<type>33</type>\n\t\t<showtype>0</showtype>\n\t\t<content />\n\t\t<url>https://mp.weixin.qq.com/mp/waerrpage?appid=wx2cb744e14895b005&amp;type=upgrade&amp;upgradetype=3#wechat_redirect</url>\n\t\t<lowurl />\n\t\t<forwardflag>0</forwardflag>\n\t\t<dataurl />\n\t\t<lowdataurl />\n\t\t<contentattr>0</contentattr>\n\t\t<streamvideo>\n\t\t\t<streamvideourl />\n\t\t\t<streamvideototaltime>0</streamvideototaltime>\n\t\t\t<streamvideotitle />\n\t\t\t<streamvideowording />\n\t\t\t<streamvideoweburl />\n\t\t\t<streamvideothumburl />\n\t\t\t<streamvideoaduxinfo />\n\t\t\t<streamvideopublishid />\n\t\t</streamvideo>\n\t\t<canvasPageItem>\n\t\t\t<canvasPageXml><![CDATA[]]></canvasPageXml>\n\t\t</canvasPageItem>\n\t\t<appattach>\n\t\t\t<attachid />\n\t\t\t<cdnthumburl>3056020100044a3048020100020301b77f02032f5aa90204dc99697102046453c16f042463663863326235312d373135652d346666332d383038372d3239393836633438343364310204011808030201000405004c53d900</cdnthumburl>\n\t\t\t<cdnthumbmd5>4abc48d8e648c7d33dbcff0f8410ad7d</cdnthumbmd5>\n\t\t\t<cdnthumblength>56038</cdnthumblength>\n\t\t\t<cdnthumbheight>576</cdnthumbheight>\n\t\t\t<cdnthumbwidth>720</cdnthumbwidth>\n\t\t\t<cdnthumbaeskey>be41bd9ee38ec7be42103c30ae8e2212</cdnthumbaeskey>\n\t\t\t<aeskey>be41bd9ee38ec7be42103c30ae8e2212</aeskey>\n\t\t\t<encryver>1</encryver>\n\t\t\t<fileext />\n\t\t\t<islargefilemsg>0</islargefilemsg>\n\t\t</appattach>\n\t\t<extinfo />\n\t\t<androidsource>0</androidsource>\n\t\t<sourceusername>gh_5afc81c5d295@app</sourceusername>\n\t\t<sourcedisplayname>优衣云选</sourcedisplayname>\n\t\t<commenturl />\n\t\t<thumburl />\n\t\t<mediatagname />\n\t\t<messageaction><![CDATA[]]></messageaction>\n\t\t<messageext><![CDATA[]]></messageext>\n\t\t<emoticongift>\n\t\t\t<packageflag>0</packageflag>\n\t\t\t<packageid />\n\t\t</emoticongift>\n\t\t<emoticonshared>\n\t\t\t<packageflag>0</packageflag>\n\t\t\t<packageid />\n\t\t</emoticonshared>\n\t\t<designershared>\n\t\t\t<designeruin>0</designeruin>\n\t\t\t<designername>null</designername>\n\t\t\t<designerrediretcturl>null</designerrediretcturl>\n\t\t</designershared>\n\t\t<emotionpageshared>\n\t\t\t<tid>0</tid>\n\t\t\t<title>null</title>\n\t\t\t<desc>null</desc>\n\t\t\t<iconUrl>null</iconUrl>\n\t\t\t<secondUrl>null</secondUrl>\n\t\t\t<pageType>0</pageType>\n\t\t\t<setKey>null</setKey>\n\t\t</emotionpageshared>\n\t\t<webviewshared>\n\t\t\t<shareUrlOriginal />\n\t\t\t<shareUrlOpen />\n\t\t\t<jsAppId />\n\t\t\t<publisherId>wxapp_wx2cb744e14895b005pages/home/index.html</publisherId>\n\t\t\t<publisherReqId />\n\t\t</webviewshared>\n\t\t<template_id />\n\t\t<md5>4abc48d8e648c7d33dbcff0f8410ad7d</md5>\n\t\t<websearch>\n\t\t\t<rec_category>0</rec_category>\n\t\t\t<channelId>0</channelId>\n\t\t</websearch>\n\t\t<weappinfo>\n\t\t\t<pagepath><![CDATA[pages/home/index.html]]></pagepath>\n\t\t\t<username>gh_5afc81c5d295@app</username>\n\t\t\t<appid>wx2cb744e14895b005</appid>\n\t\t\t<version>5</version>\n\t\t\t<type>2</type>\n\t\t\t<weappiconurl><![CDATA[http://wx.qlogo.cn/mmhead/Q3auHgzwzM7icT6HMYDzbufvCRT4UrW6WjNVoBX9Cbe3NRhLJf7IicmQ/96]]></weappiconurl>\n\t\t\t<shareId><![CDATA[1_wx2cb744e14895b005_2fb4b47c397ff6d21d817e1f7ba5a3e5_1683210610_0]]></shareId>\n\t\t\t<appservicetype>0</appservicetype>\n\t\t\t<secflagforsinglepagemode>0</secflagforsinglepagemode>\n\t\t\t<videopageinfo>\n\t\t\t\t<thumbwidth>720</thumbwidth>\n\t\t\t\t<thumbheight>576</thumbheight>\n\t\t\t\t<fromopensdk>0</fromopensdk>\n\t\t\t</videopageinfo>\n\t\t</weappinfo>\n\t\t<statextstr />\n\t\t<musicShareItem>\n\t\t\t<musicDuration>0</musicDuration>\n\t\t</musicShareItem>\n\t\t<finderLiveProductShare>\n\t\t\t<finderLiveID><![CDATA[]]></finderLiveID>\n\t\t\t<finderUsername><![CDATA[]]></finderUsername>\n\t\t\t<finderObjectID><![CDATA[]]></finderObjectID>\n\t\t\t<finderNonceID><![CDATA[]]></finderNonceID>\n\t\t\t<liveStatus><![CDATA[]]></liveStatus>\n\t\t\t<appId><![CDATA[]]></appId>\n\t\t\t<pagePath><![CDATA[]]></pagePath>\n\t\t\t<productId><![CDATA[]]></productId>\n\t\t\t<coverUrl><![CDATA[]]></coverUrl>\n\t\t\t<productTitle><![CDATA[]]></productTitle>\n\t\t\t<marketPrice><![CDATA[0]]></marketPrice>\n\t\t\t<sellingPrice><![CDATA[0]]></sellingPrice>\n\t\t\t<platformHeadImg><![CDATA[]]></platformHeadImg>\n\t\t\t<platformName><![CDATA[]]></platformName>\n\t\t\t<shopWindowId><![CDATA[]]></shopWindowId>\n\t\t\t<flashSalePrice><![CDATA[0]]></flashSalePrice>\n\t\t\t<flashSaleEndTime><![CDATA[0]]></flashSaleEndTime>\n\t\t\t<ecSource><![CDATA[]]></ecSource>\n\t\t\t<sellingPriceWording><![CDATA[]]></sellingPriceWording>\n\t\t\t<platformIconURL><![CDATA[]]></platformIconURL>\n\t\t\t<firstProductTagURL><![CDATA[]]></firstProductTagURL>\n\t\t\t<firstProductTagAspectRatioString><![CDATA[0.0]]></firstProductTagAspectRatioString>\n\t\t\t<secondProductTagURL><![CDATA[]]></secondProductTagURL>\n\t\t\t<secondProductTagAspectRatioString><![CDATA[0.0]]></secondProductTagAspectRatioString>\n\t\t\t<firstGuaranteeWording><![CDATA[]]></firstGuaranteeWording>\n\t\t\t<secondGuaranteeWording><![CDATA[]]></secondGuaranteeWording>\n\t\t\t<thirdGuaranteeWording><![CDATA[]]></thirdGuaranteeWording>\n\t\t\t<isPriceBeginShow>false</isPriceBeginShow>\n\t\t</finderLiveProductShare>\n\t\t<finderOrder>\n\t\t\t<appID><![CDATA[]]></appID>\n\t\t\t<orderID><![CDATA[]]></orderID>\n\t\t\t<path><![CDATA[]]></path>\n\t\t\t<priceWording><![CDATA[]]></priceWording>\n\t\t\t<stateWording><![CDATA[]]></stateWording>\n\t\t\t<productImageURL><![CDATA[]]></productImageURL>\n\t\t\t<products><![CDATA[]]></products>\n\t\t\t<productsCount><![CDATA[0]]></productsCount>\n\t\t</finderOrder>\n\t\t<finderShopWindowShare>\n\t\t\t<finderUsername><![CDATA[]]></finderUsername>\n\t\t\t<avatar><![CDATA[]]></avatar>\n\t\t\t<nickname><![CDATA[]]></nickname>\n\t\t\t<commodityInStockCount><![CDATA[]]></commodityInStockCount>\n\t\t\t<appId><![CDATA[]]></appId>\n\t\t\t<path><![CDATA[]]></path>\n\t\t\t<appUsername><![CDATA[]]></appUsername>\n\t\t\t<query><![CDATA[]]></query>\n\t\t\t<liteAppId><![CDATA[]]></liteAppId>\n\t\t\t<liteAppPath><![CDATA[]]></liteAppPath>\n\t\t\t<liteAppQuery><![CDATA[]]></liteAppQuery>\n\t\t\t<platformTagURL><![CDATA[]]></platformTagURL>\n\t\t\t<saleWording><![CDATA[]]></saleWording>\n\t\t\t<reputationInfo>\n\t\t\t\t<hasReputationInfo>0</hasReputationInfo>\n\t\t\t\t<reputationScore>0</reputationScore>\n\t\t\t\t<reputationWording />\n\t\t\t\t<reputationTextColor />\n\t\t\t\t<reputationLevelWording />\n\t\t\t\t<reputationBackgroundColor />\n\t\t\t</reputationInfo>\n\t\t\t<productImageURLList />\n\t\t</finderShopWindowShare>\n\t\t<findernamecard>\n\t\t\t<username />\n\t\t\t<avatar><![CDATA[]]></avatar>\n\t\t\t<nickname />\n\t\t\t<auth_job />\n\t\t\t<auth_icon>0</auth_icon>\n\t\t\t<auth_icon_url />\n\t\t\t<ecSource><![CDATA[]]></ecSource>\n\t\t</findernamecard>\n\t\t<finderGuarantee>\n\t\t\t<scene><![CDATA[0]]></scene>\n\t\t</finderGuarantee>\n\t\t<directshare>0</directshare>\n\t\t<gamecenter>\n\t\t\t<namecard>\n\t\t\t\t<iconUrl />\n\t\t\t\t<name />\n\t\t\t\t<desc />\n\t\t\t\t<tail />\n\t\t\t\t<jumpUrl />\n\t\t\t</namecard>\n\t\t</gamecenter>\n\t\t<patMsg>\n\t\t\t<chatUser />\n\t\t\t<records>\n\t\t\t\t<recordNum>0</recordNum>\n\t\t\t</records>\n\t\t</patMsg>\n\t\t<secretmsg>\n\t\t\t<issecretmsg>0</issecretmsg>\n\t\t</secretmsg>\n\t\t<referfromscene>0</referfromscene>\n\t\t<gameshare>\n\t\t\t<liteappext>\n\t\t\t\t<liteappbizdata />\n\t\t\t\t<liteapppriority>0</liteapppriority>\n\t\t\t</liteappext>\n\t\t\t<gameshareid />\n\t\t\t<sharedata />\n\t\t\t<isvideo>0</isvideo>\n\t\t\t<duration>-1</duration>\n\t\t\t<isexposed>0</isexposed>\n\t\t\t<readtext />\n\t\t</gameshare>\n\t</appmsg>\n\t<fromusername>fioman</fromusername>\n\t<scene>0</scene>\n\t<appinfo>\n\t\t<version>1</version>\n\t\t<appname></appname>\n\t</appinfo>\n\t<commenturl></commenturl>\n</msg>\n",
-  //       cover: "https://cdn.uimkit.chat/z7X7sK16QnMf4HhmYVApr.jpg"
-  //     }
-  //   });
-  //   const message = await client.sendMessage(sendReq);
-  //   console.log(JSON.stringify(message, undefined, 4));
-  // })
+  it('send miniprogram message to conversation', async () => {
+    const sendReq = client.createMiniProgramMessage({
+      conversation_id: 'kixVL6qOfz9xLcPe_xzpA',
+      miniprogram: {
+        content: "<?xml version=\"1.0\"?> <msg> <appmsg appid=\"\" sdkver=\"0\"> <title>愤怒的小蜗牛 的创作</title> <des>愤怒的小蜗牛 的创作</des> <type>33</type> <url>https://mp.weixin.qq.com/mp/waerrpage?appid=wxe66c6cf31faca8f0&amp;type=upgrade&amp;upgradetype=3#wechat_redirect</url> <appattach>\n<cdnthumburl>3051020100044a304802010002031c850902033d14bc02046bca8472020464552444042465386237366438332d663064332d343762382d626338312d6231303062386337336337380204011808030201000400</cdnthumburl> <cdnthumbmd5>e68aa7509f3cde2641d0a8ba263533bd</cdnthumbmd5> <cdnthumblength>102728</cdnthumblength> <cdnthumbwidth>720</cdnthumbwidth> <cdnthumbheight>576</cdnthumbheight> <cdnthumbaeskey>268d4862b5e44ecc3a0edc9ff9d61c47</cdnthumbaeskey> <aeskey>268d4862b5e44ecc3a0edc9ff9d61c47</aeskey> <encryver>0</encryver> <filekey>wxid_xvh6upfufvih22_413_1683301487</filekey>\n</appattach> <sourceusername>gh_011d9be46cf8@app</sourceusername> <sourcedisplayname>AI艺术玩家</sourcedisplayname> <recorditem><![CDATA[(null)]]></recorditem> <weappinfo> <username><![CDATA[gh_011d9be46cf8@app]]></username> <appid><![CDATA[wxe66c6cf31faca8f0]]></appid> <type>2</type> <version>1</version> <weappiconurl><![CDATA[http://mmbiz.qpic.cn/mmbiz_png/IHH8ZPPUpJRXvekSibUSnAIrmQW7HuozaCRqR9Aqc1zyFEYVibmzMMlG4iaxHVfibWpbQflC32u1okLNHh3e5oADlg/640?wx_fmt=png&wxfrom=200]]></weappiconurl> <pagepath><![CDATA[pages/detail/index.html?id=clhhvzrtb0011hu017a7qdb38]]></pagepath> <shareId><![CDATA[0_wxe66c6cf31faca8f0_bad255c872821c954c3a6fe6d1f5a89c_1683214569_0]]></shareId> <appservicetype>0</appservicetype> <brandofficialflag>0</brandofficialflag> <showRelievedBuyFlag>0</showRelievedBuyFlag> <flagshipflag>0</flagshipflag> <subType>0</subType> <isprivatemessage>0</isprivatemessage><weapppagethumbrawurl><![CDATA[https://aigcstore.oss-cn-shenzhen.aliyuncs.com/angryalan_On_the_way_home_from_work_late_at_night_the_exhausted_13c5fed5-2440-4256-ac15-0d2eebe2bafb.png]]></weapppagethumbrawurl></weappinfo> </appmsg> <scene>0</scene> <appinfo> <version>1</version> <appname></appname> </appinfo> <commenturl></commenturl> </msg>",
+        cover: "https://aigcstore.oss-cn-shenzhen.aliyuncs.com/angryalan_Generate_a_prompt_for_Midjourneys_artificial_intellig_df9b3fef-c0e8-48c5-a29c-d23e9f2b2587.png",
+      }
+    });
+    const message = await client.sendMessage(sendReq);
+    console.log(JSON.stringify(message, undefined, 4));
+  })
 
   // it('send file message to conversation', async () => {
   //   const sendReq = client.createFileMessage({
   //     conversation_id: 'kixVL6qOfz9xLcPe_xzpA',
-  //     upload_file: 'test/resources/test.docx',
-  //     upload_file_name: '测试文档.docx',
+  //     upload_file: 'test/resources/test_large_image.png',
+  //     upload_file_name: '测试图片.png',
   //   });
   //   const message = await client.sendMessage(sendReq);
   //   console.log(JSON.stringify(message, undefined, 4));
@@ -119,23 +119,23 @@ describe('send_messages', () => {
   //   console.log(JSON.stringify(message, undefined, 4));
   // })
 
-  it('send link attachment to conversation', async () => {
-    const sendReq = client.createLinkMessage({
-      conversation_id: 'kixVL6qOfz9xLcPe_xzpA',
-      link: {
-        url: 'https://www.163.com/money/article/I43EE07900259K2Q.html',
-        title: '巴菲特：对人工智能有点担心',
-        description: '伯克希尔哈撒韦公司CEO沃伦·巴菲特一开始打趣称自己不懂AI、机器人后，正儿八经地表示，他不认为会有AI能取代人类的基因，但AI可以做一系列非常棒的事情。',
-        image: 'https://nimg.ws.126.net/?url=http://bjnewsrec-cv.ws.126.net/little631d9d44305j00ru8ob5003jc001b800vhg.jpg&thumbnail=140y88&quality=80&type=jpg'
-      }
-    });
-    const message = await client.sendMessage(sendReq);
-    console.log(JSON.stringify(message, undefined, 4));
-  })
+  // it('send link attachment to conversation', async () => {
+  //   const sendReq = client.createLinkMessage({
+  //     conversation_id: 'kixVL6qOfz9xLcPe_xzpA',
+  //     link: {
+  //       url: 'https://www.163.com/money/article/I43EE07900259K2Q.html',
+  //       title: '巴菲特：对人工智能有点担心',
+  //       description: '伯克希尔哈撒韦公司CEO沃伦·巴菲特一开始打趣称自己不懂AI、机器人后，正儿八经地表示，他不认为会有AI能取代人类的基因，但AI可以做一系列非常棒的事情。',
+  //       image: 'https://nimg.ws.126.net/?url=http://bjnewsrec-cv.ws.126.net/little631d9d44305j00ru8ob5003jc001b800vhg.jpg&thumbnail=140y88&quality=80&type=jpg'
+  //     }
+  //   });
+  //   const message = await client.sendMessage(sendReq);
+  //   console.log(JSON.stringify(message, undefined, 4));
+  // })
 
   // it('send audio message to conversation', async () => {
   //   const sendReq = client.createAudioMessage({
-  //     conversation_id: 'YwHwFhGk9iZawmbvD4cKq',
+  //     conversation_id: 'kixVL6qOfz9xLcPe_xzpA',
   //     upload_file: 'test/resources/test_audio.m4a',
   //   });
   //   const message = await client.sendMessage(sendReq);
